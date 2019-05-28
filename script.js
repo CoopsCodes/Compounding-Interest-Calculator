b4f910ec0090335bcd93a7705cdb9d22ac877134
Cooper Viktor <im.cooperviktor@gmail.com>
2019-05-28 12:11:06 +1000

    adding css stylesheet, added bootstrap to the form, built html form in document, started adding functionality in JS

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //* A = P(1 + r/n)nt  **nt after the brackets is to a power**
-
 //! accumulated interest
 accumulatedInterest = (principal, annualRate, nPeriods, nTime) => {
   total = principal * Math.pow(1 + annualRate / nPeriods, nPeriods * nTime)
@@ -9,5 +8,14 @@ accumulatedInterest = (principal, annualRate, nPeriods, nTime) => {
 adjustInterestRate = (interestRate) => {
   return interestRate / 100
 }
+// console.log(accumulatedInterest(10000.00, adjustInterestRate(3.8), 12, 0.5));
 
-console.log(accumulatedInterest(10000.00, adjustInterestRate(3.8), 12, 0.5));
+document.querySelector('#interestCalculator').addEventListener('submit', (e) => {
+  //* prevents the form submitting the default action.
+  e.preventDefault;
+
+  principal = document.querySelector('#principal').value;
+  interestRate = document.querySelector('#interestRate').value;
+  months = document.querySelector('#months').value;
+
+})
